e974536631ea4e41b0336ba149abf45f707ac854
saYmd-moe <liuymyz@foxmail.com>
2021-10-19 22:40:59 +0800

    完成第71题（交集合）

---
+++ T071.py
@@ -0,0 +1,12 @@
+s1 = input().split(' ')
+s2 = input().split(' ')
+result = []
+
+for i in s1:
+    if i in s2:
+        result.append(i)
+    else:
+        continue
+
+result.sort()
+print(' '.join(result))
