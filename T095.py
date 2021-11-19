b363a0984236451441c9167bbf85f27e63e65a01
saYmd.moe <liuymyz@foxmail.com>
2021-11-19 09:14:59 +0800

    完成第95题（镜像字符）

---
+++ T095.py
@@ -0,0 +1,9 @@
+char = list(input())
+n = int(input())
+L = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+for i in range(n - 1, len(char)):
+    char[i] = L[25 - L.index(char[i])]
+print(''.join(char))
